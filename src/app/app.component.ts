cb8f951294f71b79e7d2257c724ca4a281ff7dbb
Matt Campbell <xenolinguist@gmail.com>
2017-03-22 20:12:57 -0500

    first draft

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+  num: number = 1;
+  dice: Array<number> = [];
+  counts: number[] = [];
+  successes: number = 0;
+
+  roll() : void {
+    this.dice = [];
+    this.counts = [0,0,0,0,0,0,0,0,0,0];
+
+    for (let i = 0; i < this.num; i++) {
+      let roll: number = Math.floor(Math.random() * 10) + 1;
+      this.dice.push(roll);
+      this.counts[roll-1] += 1;
+    }
+    this.successes = this.counts[6] + this.counts[7] + this.counts[8] + this.counts[9];
+  }
 }
