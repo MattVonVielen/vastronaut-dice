620b9fac2986bee7014399d614bc65efce232dfd
Matt Campbell <xenolinguist@gmail.com>
2017-03-26 22:44:15 -0500

    add alternate theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
+import { HostBinding, Component } from '@angular/core';
 import { DiceRoll } from './dice-roll';
 
 @Component({
-  selector: 'app-root',
+  selector: 'body',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   num: number = 1;
   rollHistory: DiceRoll[] = [];
+  @HostBinding('class') theme: string = 'brightest-eye';
 
   roll() : void {
     this.rollHistory.unshift(new DiceRoll(this.num));
+  }
+
+  changeTheme() : void {
+    if (this.theme === 'peacekeep') {
+      this.theme = 'brightest-eye';
+    } else {
+      this.theme = 'peacekeep';
+    }
   }
 }
